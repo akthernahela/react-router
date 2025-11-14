792e8793077eafc5fc2731311ca3c9b3989fa102
akthernahela <nahela00@gmail.com>
2025-11-14 12:12:34 +0100

    Add three main sections and the header components

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Header from './components/Header';
 
 
 function App() {
@@ -5,7 +6,9 @@ function App() {
 
   return (
     <>
-
+      <Header />
+      <Main />
+      <Footer />
     </>
   )
 }
