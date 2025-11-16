35ad5cdd16d0d283fe0ffdf7add73cc1cef6b463
akthernahela <nahela00@gmail.com>
2025-11-16 23:47:40 +0100

    Map the products array

--- src/shop/Products.jsx
+++ src/shop/Products.jsx
@@ -24,14 +24,17 @@ export default function Products() {
                 </div>
                 <hr />
                 <div className="row">
-                    <div class="card" style="width: 18rem;">
-
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
+                    {
+                        products.map(product => (
+                            <div class="card" style="width: 18rem;">
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                                </div>
+                            </div>
+                        ))
+                    }
                 </div>
             </div>
         </>
