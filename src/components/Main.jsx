272a815dc5872aaacd9955d0140a8a22e261dc87
akthernahela <nahela00@gmail.com>
2025-11-16 21:47:08 +0100

    Join the files

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,9 +5,17 @@ export default function Main() {
     return (
         <>
             <main className="container">
-                <div className="titleBox"></div>
-                <h1>Mix&Buy</h1>
-                <p></p>
+                <div className="titleBox">
+                    <h1>Mix&Buy</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Id, voluptatem modi officiis consequatur eveniet aut tempora in?
+                        Explicabo corporis quidem omnis commodi doloribus,
+                        neque ut aliquam, sapiente natus, nam autem.
+                    </p>
+                </div>
+                <h2>I nostri prodotti</h2>
+                <br />
             </main>
         </>
     )
--- src/shop/HomePage.jsx
+++ src/shop/HomePage.jsx
@@ -1,10 +1,13 @@
 import Header from "../components/Header";
+import Main from "../components/Main";
+
 
 export default function HomePage() {
 
     return (
         <>
             <Header />
+            <Main />
         </>
 
     )
