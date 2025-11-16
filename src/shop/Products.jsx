12bfbef169b37690087b4d63f513ad5164f6bdc7
akthernahela <nahela00@gmail.com>
2025-11-16 23:38:40 +0100

    Add an use effect function to do my ajax call

--- src/shop/Products.jsx
+++ src/shop/Products.jsx
@@ -6,7 +6,7 @@ import { useEffect, useState } from "react";
 export default function Products() {
     const prodcutsList = 'https://fakestoreapi.com/products';
     const [products, setProducts] = useState([]);
-
+    useEffect(() => { }, []);
     return (
         <>
             <Header />
