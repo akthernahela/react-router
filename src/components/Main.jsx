710085ffe615098a3c378d3291701c58b6d8752b
akthernahela <nahela00@gmail.com>
2025-11-16 18:44:24 +0100

    Add the title

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,12 @@
 import { useEffect, useState } from "react";
 
-export default function Main() { }
+export default function Main() {
+
+    return (
+        <>
+            <main className="container">
+                <h1>Mix&Buy</h1>
+            </main>
+        </>
+    )
+}
