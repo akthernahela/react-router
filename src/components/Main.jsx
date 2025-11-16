7e01a29b703e487954fa7b016d0a41cecd7211c0
akthernahela <nahela00@gmail.com>
2025-11-16 21:44:03 +0100

    Prepare the title side

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,7 +5,9 @@ export default function Main() {
     return (
         <>
             <main className="container">
+                <div className="titleBox"></div>
                 <h1>Mix&Buy</h1>
+                <p></p>
             </main>
         </>
     )
