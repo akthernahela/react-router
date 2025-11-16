6b19c32ee830578f9da3aa94166b9fd50e9b969b
akthernahela <nahela00@gmail.com>
2025-11-16 23:05:56 +0100

    Prepare pages for the products

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,4 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-
-
 export default function Main() {
-    const prodcutsList = 'https://fakestoreapi.com/products';
-
 
     return (
         <>
--- src/shop/Products.jsx
+++ src/shop/Products.jsx
@@ -1,7 +1,10 @@
 import Header from "../components/Header";
 import Main from "../components/Main";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function Products() {
+    const prodcutsList = 'https://fakestoreapi.com/products';
 
     return (
         <>
