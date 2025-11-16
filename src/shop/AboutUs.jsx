98d207aa1bc2978a4e77f62492a2ee746d3a5e90
akthernahela <nahela00@gmail.com>
2025-11-16 22:19:13 +0100

    Give the header to all the pages

--- src/shop/AboutUs.jsx
+++ src/shop/AboutUs.jsx
@@ -1,2 +1,13 @@
 import Header from "../components/Header";
 import Main from "../components/Main";
+
+export default function AboutUs() {
+
+    return (
+        <>
+            <Header />
+            <Main />
+        </>
+
+    )
+}
--- src/shop/Products.jsx
+++ src/shop/Products.jsx
@@ -1,2 +1,13 @@
 import Header from "../components/Header";
 import Main from "../components/Main";
+
+export default function Products() {
+
+    return (
+        <>
+            <Header />
+            <Main />
+        </>
+
+    )
+}
