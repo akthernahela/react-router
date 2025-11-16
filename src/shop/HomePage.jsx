78e20c54d834c1356e43222db9fdb2ffe96f5dc2
akthernahela <nahela00@gmail.com>
2025-11-16 23:03:29 +0100

    set the home page

--- src/shop/HomePage.jsx
+++ src/shop/HomePage.jsx
@@ -10,7 +10,7 @@ export default function HomePage() {
             <div class="card text-bg-dark">
                 <ShopBackground />
                 <div class="card-img-overlay">
-                    <h5 class="card-title">Card title</h5>
+                    <h1 class="card-title">Mix&Buy</h1>
                     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                     <p class="card-text"><small>Last updated 3 mins ago</small></p>
                 </div>
