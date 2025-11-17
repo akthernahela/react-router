0530111964bd456d013788061029b0529b901722
akthernahela <nahela00@gmail.com>
2025-11-17 13:28:35 +0100

    Add the space between the cols

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,26 +4,36 @@ export default function Footer() {
     return (
         <footer>
             <div className="container pt-5 pb-5">
-                <div className="row">
-                    <div className="col">
+                <div className="row mb-5 d-flex justify-content-between">
+                    <div className="col-auto">
                         <h2>Mix & Buy</h2>
                         <p>Lorem ipsum dolor sit amet.</p>
                     </div>
-                    <div className="col">
+                    <div className="col-auto">
                         <div className="d-flex flex-column">
+                            <Link to="/" className="text-decoration-none text-light mb-1" aria-current="page">Home</Link>
+                            <Link to="/aboutUs" className="text-decoration-none text-light mb-1" aria-current="page">About us</Link>
+                            <Link to="/products" className="text-decoration-none text-light mb-1" aria-current="page">Products</Link>
+                            <Link to="/" className="text-decoration-none text-light mb-1" aria-current="page">Home</Link>
+                            <Link to="/aboutUs" className="text-decoration-none text-light mb-1" aria-current="page">About us</Link>
+                            <Link to="/products" className="text-decoration-none text-light mb-1" aria-current="page">Products</Link>
                             <Link to="/" className="text-decoration-none text-light mb-1" aria-current="page">Home</Link>
                             <Link to="/aboutUs" className="text-decoration-none text-light mb-1" aria-current="page">About us</Link>
                             <Link to="/products" className="text-decoration-none text-light mb-1" aria-current="page">Products</Link>
                         </div>
                     </div>
-                    <div className="col">
+                    <div className="col-auto">
                         <div className="d-flex flex-column">
+                            <Link to="/" className="text-decoration-none text-light mb-1" aria-current="page">Home</Link>
+                            <Link to="/aboutUs" className="text-decoration-none text-light mb-1" aria-current="page">About us</Link>
+                            <Link to="/products" className="text-decoration-none text-light mb-1" aria-current="page">Products</Link>
                             <Link to="/" className="text-decoration-none text-light mb-1" aria-current="page">Home</Link>
                             <Link to="/aboutUs" className="text-decoration-none text-light mb-1" aria-current="page">About us</Link>
                             <Link to="/products" className="text-decoration-none text-light mb-1" aria-current="page">Products</Link>
                         </div>
                     </div>
                 </div>
+                <hr />
             </div>
         </footer>
     )
