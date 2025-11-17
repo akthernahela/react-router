c33620a15d16a07ea57250848bd3f39b1c744647
akthernahela <nahela00@gmail.com>
2025-11-17 16:17:09 +0100

    Import axios

--- src/shop/SingleProduct.jsx
+++ src/shop/SingleProduct.jsx
@@ -1,8 +1,11 @@
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 
 export default function SingleProduct() {
     const { id } = useParams()
+
+
     return (
         <>
             <h1>Id: {id}</h1>
