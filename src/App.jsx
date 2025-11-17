aa8846f666191a7b7b30e794fea1987f7549bc14
akthernahela <nahela00@gmail.com>
2025-11-17 16:05:41 +0100

    Add the route for the product id

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from "./shop/HomePage";
 import AboutUs from "./shop/AboutUs";
 import Products from "./shop/Products";
+import SingleProduct from './shop/SingleProduct';
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
           <Route path='/' element={<HomePage />} />
           <Route path='/aboutUs' element={<AboutUs />} />
           <Route path='/products' element={<Products />} />
+          <Route path='/products/:id' element={<SingleProduct />} />
         </Routes>
       </BrowserRouter>
 
--- src/shop/SingleProduct.jsx
+++ src/shop/SingleProduct.jsx
@@ -1,1 +1,4 @@
-export default function SingleProduct() { }
+export default function SingleProduct() {
+
+    return ()
+}
