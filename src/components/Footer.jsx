5a39c8dd91a7fb00ef5e1bcf34ee80a403cef10e
akthernahela <nahela00@gmail.com>
2025-11-17 12:51:47 +0100

    Add a row at the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,12 @@
 export default function Footer() {
 
-    return ()
+    return (
+        <footer>
+            <div className="row">
+                <div className="col"></div>
+                <div className="col"></div>
+                <div className="col"></div>
+            </div>
+        </footer>
+    )
 }
