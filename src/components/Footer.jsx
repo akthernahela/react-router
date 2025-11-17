3423df68328704dc3fde1cf47d0479596012fb94
akthernahela <nahela00@gmail.com>
2025-11-17 12:54:37 +0100

    Fix the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,16 @@
+import { Link } from "react-router-dom";
 export default function Footer() {
 
     return (
         <footer>
             <div className="row">
-                <div className="col"></div>
-                <div className="col"></div>
+                <div className="col">
+                    <h4>Mix & Buy</h4>
+                </div>
+                <div className="col">
+                    <a href="">Home</a>
+                    <a href=""></a>
+                </div>
                 <div className="col"></div>
             </div>
         </footer>
