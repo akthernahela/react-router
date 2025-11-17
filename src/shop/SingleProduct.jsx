25027888a95c9394e3cdb177b86412a7f4c5eded
akthernahela <nahela00@gmail.com>
2025-11-17 16:20:13 +0100

    Add a variable for the state

--- src/shop/SingleProduct.jsx
+++ src/shop/SingleProduct.jsx
@@ -1,9 +1,10 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 
 export default function SingleProduct() {
     const { id } = useParams()
+    const [product, setProduct] = useState({})
 
 
     return (
